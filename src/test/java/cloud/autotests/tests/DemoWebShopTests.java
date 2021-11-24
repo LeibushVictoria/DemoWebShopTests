9ab766e5b9c5dfda23e616c44251167411bc1782
Виктория Лейбуш <viktoriia.leibush@monopoly.su>
2021-11-24 22:11:08 +0300

    Revert "add allure"
    This reverts commit db12ecd6e041185e219842b0a50266da9fd9d51a.
    
    Revert "update build.gradle"
    
    This reverts commit 454009544e52f2a072fb63c2f44ae274b28587a2.
    
    Revert "add allure"
    
    This reverts commit c172131b52ab28ee9be48b4921b370579ff1cce8.

--- src/test/java/cloud/autotests/tests/DemoWebShopTests.java
+++ src/test/java/cloud/autotests/tests/DemoWebShopTests.java
@@ -1,42 +1,44 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.config.demowebshop.App;
+import cloud.autotests.config.App;
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Cookie;
 
-import static cloud.autotests.filters.CustomLogFilter.customLogFilter;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.is;
 
-public class DemoWebShopTests extends TestBase{
+public class DemoWebShopTests {
 
-    String authCookie = "";
+    @BeforeAll
+    static void configureBaseUrl() {
+        RestAssured.baseURI = App.config.apiUrl();
+        Configuration.baseUrl = App.config.webUrl();
+    }
+
+    String authCookie;
     @BeforeEach
-    public void getAndSetCookie() {
-        step("Get cookie by api and set it to browser", () -> {
-            authCookie = given()
-                    .filter(customLogFilter().withCustomTemplates())
-                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                    .formParam("Email", App.config.userLogin())
-                    .formParam("Password", App.config.userPassword())
-                    .when()
-                    .post("/login")
-                    .then()
-                    .statusCode(302)
-                    .extract()
-                    .cookie("NOPCOMMERCE.AUTH");
+    public void beforeEach() {
+        authCookie = given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("Email", App.config.userLogin())
+                .formParam("Password", App.config.userPassword())
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(302)
+                .extract()
+                .cookie("NOPCOMMERCE.AUTH");
 
-            open("/Themes/DefaultClean/Content/images/logo.png");
-
-            getWebDriver().manage().addCookie(
-                    new Cookie("NOPCOMMERCE.AUTH", authCookie));
-        });
+        open("/Themes/DefaultClean/Content/images/logo.png");
+        getWebDriver().manage().addCookie(
+                new Cookie("NOPCOMMERCE.AUTH", authCookie));
     }
 
     @Test
@@ -45,9 +47,8 @@ public class DemoWebShopTests extends TestBase{
     void addToCartTest() {
         step("Add product to cart", () -> {
             given()
-                    .filter(customLogFilter().withCustomTemplates())
                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                    .cookie("NOPCOMMERCE.AUTH")
+                    .cookie("NOPCOMMERCE.AUTH", authCookie)
                     .when()
                     .post("/addproducttocart/catalog/45/1/1")
                     .then()
@@ -65,10 +66,9 @@ public class DemoWebShopTests extends TestBase{
     @Test
     @Tag("demowebshop")
     @DisplayName("Add product to wishlist")
-    void addToWishlistTest() {
+    void wishlistTest() {
         step("Add product to wishlist", () -> {
             given()
-                    .filter(customLogFilter().withCustomTemplates())
                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                     .body("addtocart_14.EnteredQuantity=1")
                     .cookie("NOPCOMMERCE.AUTH", authCookie)
@@ -76,7 +76,8 @@ public class DemoWebShopTests extends TestBase{
                     .post("/addproducttocart/details/14/2")
                     .then()
                     .statusCode(200)
-                    .body(matchesJsonSchemaInClasspath("shemas/AddToWishlistSheme.json"));
+                    .body("success", is(true))
+                    .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
         });
 
         step("Check wishlist page", () -> {
